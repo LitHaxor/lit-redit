0c640935dffffad3cf3239d0d13f3e792c943dc0
Hasan Ahamed <officials.hasan@gmail.com>
2021-05-09 23:40:03 +0600

    4.2 CRUD- POST/mutate data

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { MyContext } from "src/types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Posts } from "../entities/Posts";
 @Resolver()
 export class PostResolver{
@@ -10,6 +10,7 @@ export class PostResolver{
     {
         return em.find(Posts,{})
     }
+    // get one
     @Query(()=> Posts, {nullable: true})
     post(
         @Arg('id',()=> Int) id: number,
@@ -17,5 +18,17 @@ export class PostResolver{
     ):Promise<Posts | null>
     {
         return em.findOne(Posts,{id})
+    }
+
+    // Post, creating
+    @Mutation(()=> Posts)
+    async createPost (
+        @Arg('title') title: string,
+        @Ctx() {em}: MyContext
+    ):Promise<Posts>
+    {
+        const post= em.create(Posts, {title});
+        await em.persistAndFlush(post);
+        return post;
     }
 }
